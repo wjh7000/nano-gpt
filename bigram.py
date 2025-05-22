311a21cbdf787ad0cded75223b5995a968458e34
Junhan <wjh7000@gmail.com>
2025-05-22 17:23:56 -0500

    update

--- bigram.py
+++ bigram.py
@@ -5,12 +5,13 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32
 block_size = 8
-max_iters = 3000
+max_iters = 5000
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embed = 32
+head_size = 16
 
 torch.manual_seed(1337)
 
@@ -57,16 +58,95 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+    def __init__(self, head_size):
+
+        super().__init__()
+
+        self.key = nn.Linear(n_embed, head_size, bias = False)
+        self.query = nn.Linear(n_embed, head_size, bias = False)
+        self.value = nn.Linear(n_embed, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        # self.k = key(self.x) # (B, T, 16)
+        # q = query(self.x) # (B, T, 16)
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        
+        wei = q @ k.transpose(-2, -1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        v = self.value(x)
+        out = wei @ v
+        return out
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embed, n_embed)
+    
+    def forward(self, x):
+        out =  torch.cat([h(x) for h in self.heads], dim = -1)
+        out = self.proj(out)
+        return out
+    
+class FeedForward(nn.Module):
+    def __init__(self, n_embed):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed, 4 * n_embed),
+            nn.ReLU(),
+            nn.Linear(4 * n_embed, n_embed)
+        )
+    def forward(self, x):
+        return self.net(x)
+    
+class Block(nn.Module):
+    def __init__(self, n_embed, n_head):
+        super().__init__()
+        head_size = n_embed // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embed)
+        self.ln1 = nn.LayerNorm(n_embed)
+        
+
+    def forward(self, x):
+        x = x + self.sa(x)
+        x = x + self.ffwd(x)
+        return x
+
+    
 
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
+        self.position_embedding_table = nn.Embedding(block_size, n_embed)
+        # self.sa_head = Head(n_embed)
+        # self.sa_heads = MultiHeadAttention(4, n_embed // 4)
+        # self.ffwd = FeedForward(n_embed)
+        self.block = nn.Sequential(
+            Block(n_embed, n_head=4),
+            Block(n_embed, n_head=4),
+            Block(n_embed, n_head=4),
+        )
+
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
+
     def forward(self, idx, targets=None):
+        B, T = idx.shape
+
         token_emb = self.token_embedding_table(idx) #(B, T, C)
-        logits = self.lm_head(token_emb) #(B, T, vocab_size)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
+        x = token_emb + pos_emb
+        # x = self.sa_heads(x)  # apply one head self-attention, (B, T, C)
+        # x = self.ffwd(x)
+        x = self.block(x)
+        logits = self.lm_head(x) #(B, T, vocab_size)
 
         if targets == None:
             loss = None
@@ -83,12 +163,20 @@ class BigramLanguageModel(nn.Module):
     
     def generate(self, idx, max_new_tokens):
         for _ in range(max_new_tokens):
-            logits, loss = self(idx)
-            logits = logits[:, -1, :]
+
+            idx_cond = idx[:, -block_size:]  #crop input idx
+
+            logits, loss = self(idx_cond)
+            logits = logits[:, -1, :]    # become (B, C)
             probs = F.softmax(logits, dim = -1)
             idx_next = torch.multinomial(probs, num_samples=1)
             idx = torch.cat((idx,idx_next), dim = 1)
         return idx 
+    
+
+
+    
+
     
 
 
@@ -112,6 +200,9 @@ for iter in range(max_iters):
     optimizer.step()
 
 context = torch.zeros((1, 1), dtype = torch.long, device = device)
+# context = torch.tensor(([10], [10], [10], [10]), dtype = torch.long, device=device)
+print(decode(context.view(-1).tolist()))
+print('\n')
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
 
 
